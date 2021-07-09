9c52473441aec4452e0a04b65ab2a51ad0f87625
Sorin <jordache_sor@yahoo.com>
2021-07-09 12:24:02 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
       caches.open('cache').then(function(cache) {
         return cache.addAll([
-            "/",
+            "https://carpenterx.github.io/underlords-compare-heroes/index.html",
             
          ]);
       })
