fff7750cca69ab3fa0b1a1faa38eefdf3c95ba57
Sorin <jordache_sor@yahoo.com>
2021-07-09 11:34:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 if ('serviceWorker' in navigator) {
 
     // register service worker
-    navigator.serviceWorker.register('service-worker.js');
+    navigator.serviceWorker.register('/service-worker.js');
 }
 
 let mirrorFragment = "-mirror";
